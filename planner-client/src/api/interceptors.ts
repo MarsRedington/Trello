e80498c05ba644cc164c26cf2aec5006479087a6
Red <a123@Reds-MacBook-Pro.local>
2024-04-27 10:56:36 -0600

    Trello

---
+++ planner-client/src/api/interceptors.ts
@@ -0,0 +1,58 @@
+import axios, { type CreateAxiosDefaults } from 'axios';
+
+import { errorCatch } from './error';
+import {
+	getAccessToken,
+	removeFromStorage
+} from '@/services/auth-token.service';
+import { authService } from '@/services/auth.service';
+
+const options: CreateAxiosDefaults = {
+	baseURL: 'http://localhost:4200/api',
+	headers: {
+		'Content-Type': 'application/json',
+		'Access-Control-Allow-Origin': 'http://localhost:4200',
+		'Access-Control-Allow-Credentials': 'true',
+		'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+	},
+	withCredentials: true // для серверных куков
+};
+
+const axiosClassic = axios.create(options);
+const axiosWithAuth = axios.create(options);
+
+axiosWithAuth.interceptors.request.use(config => {
+	const accessToken = getAccessToken();
+
+	if (config?.headers && accessToken)
+		config.headers.Authorization = `Bearer ${accessToken}`;
+
+	return config;
+});
+
+axiosWithAuth.interceptors.response.use(
+	config => config,
+	async error => {
+		const originalRequest = error.config;
+
+		if (
+			(error?.response?.status === 401 ||
+				errorCatch(error) === 'jwt expired' ||
+				errorCatch(error) === 'jwt must be provided') &&
+			error.config &&
+			!error.config._isRetry
+		) {
+			originalRequest._isRetry = true;
+			try {
+				await authService.getNewTokens();
+				return axiosWithAuth.request(originalRequest);
+			} catch (error) {
+				if (errorCatch(error) === 'jwt expired') removeFromStorage();
+			}
+		}
+
+		throw error;
+	}
+);
+
+export { axiosClassic, axiosWithAuth };
